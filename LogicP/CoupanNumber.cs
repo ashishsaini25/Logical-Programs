3dd10e1aeb637c643ab4ecdfb80369b40af51be3
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-24 13:28:27 +0530

    [Ashish] Add: Coupan Number Problem Solved

---
+++ LogicP/CoupanNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicP
+{
+    public class CoupanNumber
+    {
+        public void Generate()
+        {
+            Console.WriteLine("Enter the Number of Unique Coupans That You Want\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            HashSet<int> numbers = new HashSet<int>();
+            bool flag = true;
+            int ans = 0;
+            Random random = new Random();
+            while (flag)
+            {
+                int coupan=random.Next(1,1000);
+        //        Console.WriteLine(coupan);
+                if (!numbers.Contains(coupan))
+                {
+                    ans++;
+                    numbers.Add(coupan);
+                    
+                }
+                if (ans == n) break;
+            }
+            Console.Write("Total Random Number Needed to Generate {0} Unique coupans ", n);
+            Console.WriteLine(ans);
+
+        }
+    }
+}
